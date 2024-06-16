cca305217c0d9aa728c0bde1ffbf3d92355d7fe6
Arcthuruss <73853221+Arcthuruss@users.noreply.github.com>
2024-06-16 17:11:02 +0200

    vroom

---
+++ src/main/java/xyz/nahidwin/lot5/controler/CtrlBillet.java
@@ -0,0 +1,19 @@
+package xyz.nahidwin.lot5.controler;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
+public class CtrlBillet {
+    @FXML CheckBox cbSelection;
+    @FXML Label txtNom;
+    @FXML Label txtPrenom;
+    @FXML Label txtVille;
+    @FXML Label txtPayement;
+    @FXML Label txtDateReserv;
+    @FXML Label txtDate;
+    @FXML Button btnDetail;
+
+    public void initialize() {}
+}
